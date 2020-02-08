1ca5801d7d168b242f78aa4bf75eb437240e229c
Arne Schwarck <arneschwarck@gmail.com>
2020-02-08 11:20:09 +0100

    remove all trace of op_params

--- selfdrive/data_collection/gps_uploader.py
+++ selfdrive/data_collection/gps_uploader.py
@@ -3,7 +3,7 @@ import json
 import string
 import random
 import os
-from common.params import Params
+#from common.params import Params
 #from common.op_params import opParams
 #op_params = opParams()
 uniqueID = "mapd"#op_params.get('uniqueID', None)
@@ -11,24 +11,26 @@ uniqueID = "mapd"#op_params.get('uniqueID', None)
 def upload_data():
   filepath = "/data/openpilot/selfdrive/data_collection/gps-data"
   if os.path.isfile(filepath):
-    if uniqueID is None:
-      op_params.put('uniqueID', ''.join([random.choice(string.ascii_lowercase+string.ascii_uppercase+string.digits) for i in range(15)]))
-    try:
-      username = op_params.get('uniqueID', None)
-      try:
-        with open("/data/data/ai.comma.plus.offroad/files/persistStore/persist-auth", "r") as f:
-          auth = json.loads(f.read())
-        auth = json.loads(auth['commaUser'])
-        if auth and str(auth['username']) != "":
-          username = str(auth['username'])
-      except:
-        pass
+    #if uniqueID is None:
+    #  op_params.put('uniqueID', ''.join([random.choice(string.ascii_lowercase+string.ascii_uppercase+string.digits) for i in range(15)]))
+    #try:
+    #  username = op_params.get('uniqueID', None)
+    #  try:
+    #    with open("/data/data/ai.comma.plus.offroad/files/persistStore/persist-auth", "r") as f:
+    #      auth = json.loads(f.read())
+    #    auth = json.loads(auth['commaUser'])
+    #    if auth and str(auth['username']) != "":
+    #      username = str(auth['username'])
+    #  except:
+    #    pass
 
-      params = Params()
-      car = params.get('CachedFingerprint')
-      if car is not None:
-        car = json.loads(car)
-        username+="-{}".format(car[0])
+      #params = Params()
+      #car = params.get('CachedFingerprint')
+      #if car is not None:
+      #  car = json.loads(car)
+      #  username+="-{}".format(car[0])
+      car = "car"
+      username = "username"
 
       filename = "gps-data.{}".format(random.randint(1,99999))
 
