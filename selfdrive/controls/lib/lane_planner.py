b7b62d7602d49fe13947924ecae61e6e61212e15
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2020-07-25 08:27:56 -0700

    fix up the virtual_lane_code

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -88,23 +88,17 @@ class LanePlanner():
     self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
     current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
     self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
+    if not op_params.get('use_virutual_middle_line', False):
+      if self.lane_width_estimate < 2.0:
+        self.r_poly[3] -= 2.0 - self.lane_width_estimate # TODO: this should be l_poly if isRHD
+        self.lane_width_estimate = 2.0
+      elif self.lane_width_estimate > 4.0:
+        self.l_poly[3] -= self.lane_width_estimate/2 # TODO: this should be r_poly if isRHD
+        self.lane_width_estimate = self.lane_width_estimate/2
     speed_lane_width = interp(v_ego, [0., 14., 20.], [2.5, 3., 3.5]) # German Standards
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
                       (1 - self.lane_width_certainty) * speed_lane_width
-    #print(current_lane_width)
-    if not op_params.get('use_virutual_middle_line', False):
-      lane_width = self.lane_width
-      if current_lane_width < 2.0:
-        self.r_poly[3] -= 2.0 - current_lane_width # TODO: this should be l_poly if isRHD
-        #self.p_poly[3] += (2.0 - current_lane_width)/2
-        self.lane_width = 2.0
-      elif current_lane_width > 4.0:
-        self.l_poly[3] -= current_lane_width/2 # TODO: this should be r_poly if isRHD
-        #self.p_poly[3] -= current_lane_width/4
-        #self.lane_width = 4.0
-    else:
-      lane_width = min(4.0, self.lane_width)
-    self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, lane_width, v_ego)
+    self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, min(4.0, self.lane_width), v_ego)
 
   def update(self, v_ego, md):
     self.parse_model(md)
