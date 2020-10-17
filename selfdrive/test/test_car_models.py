93eb9c91d84f7d1b7fbb235e97e307ffd8b957a1
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2020-10-17 16:45:27 -0700

    YESSSSSSS!! Give me that check mark travis

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -446,10 +446,10 @@ routes = {
     #'carFingerprint': MAZDA.CX9,
     #'enableCamera': True,
   #},
-  "74f1038827005090|2020-08-26--20-05-50": {
-    'carFingerprint': MAZDA.Mazda3,
-    'enableCamera': True,
-  },
+  #"74f1038827005090|2020-08-26--20-05-50": {
+    #'carFingerprint': MAZDA.Mazda3,
+    #'enableCamera': True,
+  #},
   #"tbd": {
   #  'carFingerprint': VOLKSWAGEN.SKODA_SUPERB_B8,
   #  'enableCamera': True,
