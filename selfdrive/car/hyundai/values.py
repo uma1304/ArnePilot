aa53a266dbb05563afd4111eae9b21d3cc0b61a9
Kumar <36933347+rav4kumar@users.noreply.github.com>
2020-11-03 13:30:24 -0700

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -181,7 +181,7 @@ FW_VERSIONS = {
     (Ecu.eps, 0x7d4, None): [b'\xf1\x00TM  MDPS C 1.00 1.00 56340-S2000 8409'],
     (Ecu.engine, 0x7e0, None): [b'\xf1\x81606EA051\x00\x00\x00\x00\x00\x00\x00\x00'],
   },
-  CAR.KIA_STINGER: { 
+  CAR.KIA_STINGER: {
     (Ecu.eps, 0x7d4, None): [b'\xf1\x00CK  MDPS R 1.00 1.04 57700-J5420 4C4VL104'],
   },
   CAR.KIA_OPTIMA_HEV: {
