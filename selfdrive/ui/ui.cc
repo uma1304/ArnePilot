af313748ac5b7327bf122d9c6ccf5e4f2f2a7546
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2020-11-11 12:09:20 -0700

    typo

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -194,7 +194,7 @@ void update_sockets(UIState *s) {
   }
   if (sm.updated("thermal")) {
     scene.thermal = sm["thermal"].getThermal();
-    scene.pa0 = scene.thermal.getbat();
+    scene.bat = scene.thermal.getbat();
   }
   if (sm.updated("ubloxGnss")) {
     auto data = sm["ubloxGnss"].getUbloxGnss();
