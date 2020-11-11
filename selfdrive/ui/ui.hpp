b825ecfedb76c3b2153eb80a1d4acd03bb35c2a0
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2020-11-11 11:37:37 -0700

    ui update

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -87,6 +87,9 @@ static std::map<UIStatus, NVGcolor> bg_colors = {
 
 typedef struct UIScene {
 
+  int dfButtonStatus;
+  bool mlButtonEnabled;
+
   float mpc_x[50];
   float mpc_y[50];
 
@@ -185,6 +188,7 @@ typedef struct UIState {
   int img_network[6];
 
   SubMaster *sm;
+  PubMaster *pm;
 
   Sound *sound;
   UIStatus status;
@@ -214,6 +218,7 @@ typedef struct UIState {
   bool ignition;
   bool is_metric;
   bool longitudinal_control;
+  bool ui_debug;
   uint64_t last_athena_ping;
   uint64_t started_frame;
 
@@ -227,6 +232,7 @@ typedef struct UIState {
 } UIState;
 
 void ui_init(UIState *s);
+void sa_init(UIState *s, bool full_init);
 void ui_update(UIState *s);
 
 int write_param_float(float param, const char* param_name, bool persistent_param = false);
