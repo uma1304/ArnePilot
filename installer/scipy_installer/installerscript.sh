77df2b7b2681b92f15fdcce7eec3956155ecff1f
Arne Schwarck <arneschwarck@gmail.com>
2020-11-15 22:10:44 +0100

    Update installerscript.sh

--- installer/scipy_installer/installerscript.sh
+++ installer/scipy_installer/installerscript.sh
@@ -23,8 +23,8 @@ apt install -y --allow-unauthenticated opencv
 apt install -y --allow-unauthenticated scipy
 apt install gcc-8
 setupclang-gfort-8
-python3 -m pip install scipy
 python3 -m pip install overpy
+python3 -m pip install scipy
 # python3 -m pip install requests
 # python3 -m pip install pyzmq
 # python3 -m pip install pycapnp
