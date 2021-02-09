303383b8ca6f73408fe1a6b079300217ce4daa53
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-02-09 10:57:01 -0700

    flash the panda

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -7,9 +7,9 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 class SteerLimitParams:
   STEER_MAX = 1500
-  STEER_DELTA_UP = 13       # 1.5s time to peak torque
+  STEER_DELTA_UP = 10       # 1.5s time to peak torque
   STEER_DELTA_DOWN = 44     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
-  STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
+  STEER_ERROR_MAX = 450     # max delta between torque cmd and torque motor
 
 class CAR:
   PRIUS = "TOYOTA PRIUS 2017"
