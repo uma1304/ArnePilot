65c376846332d5ab7e8e17291b0f2acb962855d4
Arne Schwarck <arneschwarck@gmail.com>
2021-03-02 11:35:48 +0100

    Rav4h specific waitingMode fix

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -767,6 +767,14 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
       AlertStatus.normal, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .4, 2., 0.2),
   },
+  
+  EventName.waitingMode: {
+    ET.WARNING: Alert(
+      "WAITING...",
+      "Press gas/resume to gain full control!",
+      AlertStatus.userPrompt, AlertSize.mid,
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.chimeWarning1, .4, 2., 0.2),
+  },
 
   # dp
   EventName.preLaneChangeLeftALC: {
