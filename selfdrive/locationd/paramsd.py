8236aecea89dbf5834cd8f846d0b6da20df7c268
Arne Schwarck <arneschwarck@gmail.com>
2021-03-03 19:54:34 +0100

    fix syntax

--- selfdrive/locationd/paramsd.py
+++ selfdrive/locationd/paramsd.py
@@ -88,7 +88,7 @@ def main(sm=None, pm=None):
       cloudlog.info("Parameter learner found parameters for wrong car.")
       params = None
   if 'angleOffsetAverage' not in params:
-    if 'angleOffsetAverageDeg' in params
+    if 'angleOffsetAverageDeg' in params:
       params['angleOffsetAverage'] = params['angleOffsetAverageDeg']
       params.pop('angleOffsetAverageDeg')
   if (params is not None) and not all((
