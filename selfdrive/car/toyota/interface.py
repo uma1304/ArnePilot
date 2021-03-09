5835a1186cab40ba4f41ae1bc3855c60bef5a227
Arne Schwarck <arneschwarck@gmail.com>
2021-03-09 16:32:16 +0100

    See if dp is conflicting

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -465,6 +465,7 @@ class CarInterface(CarInterfaceBase):
     else:
       self.cp.update_strings(can_strings)
       ret = self.CS.update(self.cp, self.cp_cam, self.frame)
+    print("interface before speed = " + str(ret.cruiseState.speed * 3.6))
     # dp
     self.dragonconf = dragonconf
     ret.cruiseState.enabled = common_interface_atl(ret, dragonconf.dpAtl)
