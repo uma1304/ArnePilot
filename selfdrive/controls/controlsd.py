e9ab6a642717f58de769758153d10aae8d53a62f
Arne Schwarck <arneschwarck@gmail.com>
2021-03-09 12:43:01 +0100

    Fix self

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -347,7 +347,7 @@ class Controls:
   def state_transition(self, CS):
     """Compute conditional state transitions and execute actions on state transitions"""
     if self.v_cruise_kph_last != self.v_cruise_kph:
-      print("v_cruise_kph_last = " + str(v_cruise_kph_last) + " v_cruise_kph = " + str(v_cruise_kph))
+      print("v_cruise_kph_last = " + str(self.v_cruise_kph_last) + " v_cruise_kph = " + str(self.v_cruise_kph))
     self.v_cruise_kph_last = self.v_cruise_kph
 
     # if stock cruise is completely disabled, then we can use our own set speed logic
