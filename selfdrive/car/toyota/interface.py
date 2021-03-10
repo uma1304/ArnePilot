5ca49388f91dd1a5f9814d50b7e33abc1ea11659
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2021-03-09 22:42:26 -0800

    Update interface.py
    added spairrowtuning param. some corollatss2 perfer indi over pif

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -14,7 +14,7 @@ GearShifter = car.CarState.GearShifter
 EventName = car.CarEvent.EventName
 
 op_params = opParams()
-#spairrowtuning = op_params.get('spairrowtuning')
+spairrowtuning = op_params.get('spairrowtuning')
 #corolla_tss2_d_tuning = op_params.get('corolla_tss2_d_tuning')
 prius_pid = op_params.get('prius_pid')
 
@@ -299,21 +299,24 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       ret.steerActuatorDelay = 0.48
       ret.steerLimitTimer = 5.0
-      #ret.lateralTuning.init('indi')
-      #ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
-      #ret.lateralTuning.indi.innerLoopGainV = [9, 12, 15]
-      #ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
-      #ret.lateralTuning.indi.outerLoopGainV = [8, 11, 14.99]
-      #ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
-      #ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
-      #ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
-      #ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
-      ret.lateralTuning.pid.kpBP = [0.0]
-      ret.lateralTuning.pid.kiBP = [0.0]
-      ret.lateralTuning.pid.kpV = [0.028]
-      ret.lateralTuning.pid.kiV = [0.0012]
-      ret.lateralTuning.pid.kf = 0.000153263811757641 # hardcoded in latcontrol_pid, this does nothing for now
-      ret.lateralTuning.pid.newKfTuned = True
+      if spairrowtuning:
+        ret.lateralTuning.init('indi')
+        ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
+        ret.lateralTuning.indi.innerLoopGainV = [9, 12, 15]
+        ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
+        ret.lateralTuning.indi.outerLoopGainV = [8, 11, 14.99]
+        ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
+        ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
+        ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
+        ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
+        ret.steerActuatorDelay = 0.42
+      else:
+        ret.lateralTuning.pid.kpBP = [0.0]
+        ret.lateralTuning.pid.kiBP = [0.0]
+        ret.lateralTuning.pid.kpV = [0.028]
+        ret.lateralTuning.pid.kiV = [0.0012]
+        ret.lateralTuning.pid.kf = 0.000153263811757641 # hardcoded in latcontrol_pid, this does nothing for now
+        ret.lateralTuning.pid.newKfTuned = True
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
