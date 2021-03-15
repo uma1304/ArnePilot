734c84a872182278a1d3de07700c7d51ecb0aefb
Arne Schwarck <arneschwarck@gmail.com>
2021-03-15 21:20:37 +0100

    add spairrowtuning to rav4h

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -122,25 +122,27 @@ class CarInterface(CarInterfaceBase):
       if ret.enableGasInterceptor:
         ret.longitudinalTuning.kpV = [0.4, 0.36, 0.325]  # arne's tune.
         ret.longitudinalTuning.kiV = [0.195, 0.10]
-      #ret.lateralTuning.init('lqr')
-      #ret.lateralTuning.lqr.scale = 1500.0
-      #ret.lateralTuning.lqr.ki = 0.05
-      #ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-      #ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-      #ret.lateralTuning.lqr.c = [1., 0.]
-      #ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-      #ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-      #ret.lateralTuning.lqr.dcGain = 0.002237852961363602
-      ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
-      ret.lateralTuning.indi.innerLoopGainV = [9, 12, 15]
-      ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
-      ret.lateralTuning.indi.outerLoopGainV = [8, 11, 14.99]
-      ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
-      ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
-      ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
-      ret.steerActuatorDelay = 0.42
+      if spairrowtuning:
+        ret.lateralTuning.init('indi')
+        ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
+        ret.lateralTuning.indi.innerLoopGainV = [9, 12, 15]
+        ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
+        ret.lateralTuning.indi.outerLoopGainV = [8, 11, 14.99]
+        ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
+        ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
+        ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
+        ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
+        ret.steerActuatorDelay = 0.4
+      else:
+        ret.lateralTuning.init('lqr')
+        ret.lateralTuning.lqr.scale = 1500.0
+        ret.lateralTuning.lqr.ki = 0.05
+        ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+        ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+        ret.lateralTuning.lqr.c = [1., 0.]
+        ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+        ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+        ret.lateralTuning.lqr.dcGain = 0.002237852961363602
 
     elif candidate == CAR.COROLLA:
       stop_and_go = False
