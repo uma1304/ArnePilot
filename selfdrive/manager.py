6c5e2e2cea376234565d2d9cdcc720bbcdc03d39
Arne Schwarck <arneschwarck@gmail.com>
2021-03-17 12:44:49 +0100

    Add DistanceTraveled

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -633,6 +633,9 @@ def main():
     ("OpenpilotEnabledToggle", "1"),
     ("LaneChangeEnabled", "1"),
     ("IsDriverViewEnabled", "0"),
+    ("DistanceTraveled", "0"),
+    ("DistanceTraveledEngaged", "0"),
+    ("DistanceTraveledOverride", "0"),
   ]
 
   # set unset params
