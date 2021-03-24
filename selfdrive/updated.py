4d371a874c8ebee0b15d4669b649b0e1c6e8e4e9
Arne Schwarck <arneschwarck@gmail.com>
2021-03-24 14:34:25 +0100

    Update updated.py

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -325,7 +325,7 @@ def auto_update_reboot(time_offroad, need_reboot, new_version):
     if 'already up to date' not in run(NICE_LOW_PRIORITY + ["git", "pull"]).lower():
       need_reboot = True
   if time.time() - time_offroad > min_reboot_time * 60 and need_reboot:  # allow reboot x minutes after stopping openpilot or starting EON
-    os.system('reboot')
+    run(["am", "start", "-a", "android.intent.action.REBOOT"])
   return need_reboot
 
 
