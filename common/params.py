acdab3845bdd820ed8d53f7b794b98a32ef08139
Arne Schwarck <arneschwarck@gmail.com>
2021-03-24 16:16:44 +0100

    fix import error

--- common/params.py
+++ common/params.py
@@ -4,6 +4,7 @@ except ModuleNotFoundError:
   import subprocess
   subprocess.check_call(["scons", "-u", "-c", "."])
   subprocess.check_call(["scons", "-u", "-j4", "."])
+  from common.params_pyx import Params, UnknownKeyName, put_nonblocking # pylint: disable=no-name-in-module, import-error
 assert Params
 assert UnknownKeyName
 assert put_nonblocking
