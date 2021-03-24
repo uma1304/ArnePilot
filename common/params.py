d67e8202e7f91800a0db31fa53e4b6c79316d56c
Arne Schwarck <arneschwarck@gmail.com>
2021-03-24 17:54:48 +0100

    Update params.py

--- common/params.py
+++ common/params.py
@@ -1,9 +1,4 @@
-#try:
 from common.params_pyx import Params, UnknownKeyName, put_nonblocking # pylint: disable=no-name-in-module, import-error
-#except ModuleNotFoundError:
-#  import subprocess
-#  subprocess.check_call(["scons", "-u"])
-#  from common.params_pyx import Params, UnknownKeyName, put_nonblocking # pylint: disable=no-name-in-module, import-error
 assert Params
 assert UnknownKeyName
 assert put_nonblocking
