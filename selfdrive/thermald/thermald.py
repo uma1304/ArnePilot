1a15cdf94085c20b3b31c385efce33b0d5df14b0
Arne Schwarck <arneschwarck@gmail.com>
2021-03-24 14:27:29 +0100

    add back must update warning

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -195,7 +195,8 @@ def thermald_thread():
   handle_fan = None
   is_uno = False
   has_relay = False
-
+  time_valid_prev = True
+  
   params = Params()
   power_monitor = PowerMonitoring()
   no_panda_cnt = 0
@@ -327,45 +328,44 @@ def thermald_thread():
     # **** starting logic ****
 
     # Check for last update time and display alerts if needed
-    # now = datetime.datetime.utcnow()
+    now = datetime.datetime.utcnow()
     #
-    # # show invalid date/time alert
-    # startup_conditions["time_valid"] = (now.year > 2020) or (now.year == 2020 and now.month >= 10)
-    # set_offroad_alert_if_changed("Offroad_InvalidTime", (not startup_conditions["time_valid"]))
+    # show invalid date/time alert
+    time_valid = now.year >= 2019
+    if time_valid and not time_valid_prev:
+      params.delete("Offroad_InvalidTime")
+    if not time_valid and time_valid_prev:
+      put_nonblocking("Offroad_InvalidTime", json.dumps(OFFROAD_ALERTS["Offroad_InvalidTime"]))
+    time_valid_prev = time_valid
     #
-    # # Show update prompt
-    # try:
-    #   last_update = datetime.datetime.fromisoformat(params.get("LastUpdateTime", encoding='utf8'))
-    # except (TypeError, ValueError):
-    #   last_update = now
-    # dt = now - last_update
-    #
-    # update_failed_count = params.get("UpdateFailedCount")
-    # update_failed_count = 0 if update_failed_count is None else int(update_failed_count)
-    # last_update_exception = params.get("LastUpdateException", encoding='utf8')
-    #
-    # if update_failed_count > 15 and last_update_exception is not None:
-    #   if current_branch in ["release2", "dashcam"]:
-    #     extra_text = "Ensure the software is correctly installed"
-    #   else:
-    #     extra_text = last_update_exception
-    #
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
-    #   set_offroad_alert_if_changed("Offroad_UpdateFailed", True, extra_text=extra_text)
-    # elif dt.days > DAYS_NO_CONNECTIVITY_MAX and update_failed_count > 1:
-    #   set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", True)
-    # elif dt.days > DAYS_NO_CONNECTIVITY_PROMPT:
-    #   remaining_time = str(max(DAYS_NO_CONNECTIVITY_MAX - dt.days, 0))
-    #   set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", True, extra_text=f"{remaining_time} days.")
-    # else:
-    #   set_offroad_alert_if_changed("Offroad_UpdateFailed", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeeded", False)
-    #   set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
+    # Show update prompt
+    try:
+      last_update = datetime.datetime.fromisoformat(params.get("LastUpdateTime", encoding='utf8'))
+    except (TypeError, ValueError):
+      last_update = now
+    dt = now - last_update
+
+    update_failed_count = params.get("UpdateFailedCount")
+    update_failed_count = 0 if update_failed_count is None else int(update_failed_count)
+
+    if dt.days > DAYS_NO_CONNECTIVITY_MAX and update_failed_count > 1:
+      if current_connectivity_alert != "expired":
+        current_connectivity_alert = "expired"
+        params.delete("Offroad_ConnectivityNeededPrompt")
+        put_nonblocking("Offroad_ConnectivityNeeded", json.dumps(OFFROAD_ALERTS["Offroad_ConnectivityNeeded"]))
+    elif dt.days > DAYS_NO_CONNECTIVITY_PROMPT:
+      remaining_time = str(max(DAYS_NO_CONNECTIVITY_MAX - dt.days, 0))
+      if current_connectivity_alert != "prompt" + remaining_time:
+        current_connectivity_alert = "prompt" + remaining_time
+        alert_connectivity_prompt = copy.copy(OFFROAD_ALERTS["Offroad_ConnectivityNeededPrompt"])
+        alert_connectivity_prompt["text"] += remaining_time + " days."
+        params.delete("Offroad_ConnectivityNeeded")
+        put_nonblocking("Offroad_ConnectivityNeededPrompt", json.dumps(alert_connectivity_prompt))
+    elif current_connectivity_alert is not None:
+      current_connectivity_alert = None
+      params.delete("Offroad_ConnectivityNeeded")
+      params.delete("Offroad_ConnectivityNeededPrompt")
+      
     startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get("DisableUpdates") == b"1"
     startup_conditions["not_uninstalling"] = not params.get("DoUninstall") == b"1"
     startup_conditions["accepted_terms"] = params.get("HasAcceptedTerms") == terms_version
