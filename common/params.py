70bd22b7aa5dfe2c12c28e7a2182af1552a6a33f
Arne Schwarck <arneschwarck@gmail.com>
2021-03-24 17:26:42 +0100

    Update params.py

--- common/params.py
+++ common/params.py
@@ -3,8 +3,7 @@ try:
 except ModuleNotFoundError:
   import subprocess
   subprocess.check_call(["scons", "-u"])
-  from selfdrive.hardware import HARDWARE
-  HARDWARE.reboot()
+  from common.params_pyx import Params, UnknownKeyName, put_nonblocking # pylint: disable=no-name-in-module, import-error
 assert Params
 assert UnknownKeyName
 assert put_nonblocking
