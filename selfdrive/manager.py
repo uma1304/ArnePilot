a0a47ff3cfe2de59b31874a00926056593764a59
Arne Schwarck <arneschwarck@gmail.com>
2021-03-29 15:31:01 +0200

    fix github actions complaint

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -531,7 +531,7 @@ def manager_thread():
     if msg.thermal.freeSpace < 0.05:
       logger_dead = True
 
-    if msg.thermal.started or (run_all := False):
+    if msg.thermal.started:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
           kill_managed_process(p)
