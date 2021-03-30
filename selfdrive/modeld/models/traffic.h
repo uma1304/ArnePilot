4dd7fce18e4b9a1118349174eaf611878d33e0d3
Comma Device <device@comma.ai>
2021-03-30 07:28:11 -0500

    rm libs from header

--- selfdrive/modeld/models/traffic.h
+++ selfdrive/modeld/models/traffic.h
@@ -1,13 +1,3 @@
-#include <SNPE/SNPE.hpp>
-#include <SNPE/SNPEBuilder.hpp>
-#include <SNPE/SNPEFactory.hpp>
-#include <DlContainer/IDlContainer.hpp>
-#include <DlSystem/DlError.hpp>
-#include <DlSystem/ITensor.hpp>
-#include <DlSystem/ITensorFactory.hpp>
-#include <DlSystem/IUserBuffer.hpp>
-#include <DlSystem/IUserBufferFactory.hpp>
-
 #include <string>
 #include <sstream>
 #include <iostream>
