3acea5aa2d9d7d0fdc4250f67a6e46c4f2560430
Comma Device <device@comma.ai>
2021-03-30 07:30:15 -0500

    rm libs from header

--- selfdrive/modeld/models/traffic.h
+++ selfdrive/modeld/models/traffic.h
@@ -7,21 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <cstdio>
-#include <cstdlib>
-#include <cstdint>
-#include <unistd.h>
-#include <assert.h>
 
 #include "common/visionbuf.h"
 #include "common/visionipc.h"
 #include "common/timing.h"
 #include "messaging.hpp"
 #include "runners/run.h"
-
-//#include "cereal/gen/cpp/log.capnp.h"
-
-//#include <capnp/message.h>
-//#include <capnp/serialize-packed.h>
 
 int main();
