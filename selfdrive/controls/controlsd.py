rav4kumar/openpilot

Turn Controller: Provide longitudinal control on turns via a solution to Planner

selfdrive/controls/controlsd.py
@@ -492,7 +492,7 @@ class Controls:
     controlsState.jerkFactor = float(self.sm['plan'].jerkFactor)
     controlsState.gpsPlannerActive = self.sm['plan'].gpsPlannerActive
     controlsState.vCurvature = self.sm['plan'].vCurvature
-    controlsState.decelForModel = self.sm['plan'].longitudinalPlanSource == LongitudinalPlanSource.model
+    controlsState.decelForModel = self.sm['plan'].decelForTurn
     controlsState.cumLagMs = -self.rk.remaining * 1000.
     controlsState.startMonoTime = int(start_time * 1e9)
     controlsState.mapValid = self.sm['plan'].mapValid

uma1304/ArnePilot

selfdrive/controls/controlsd.py
@@ -523,6 +523,7 @@ class Controls:
     controlsState.ufAccelCmd = float(self.LoC.pid.f)
     controlsState.vTargetLead = float(v_acc)
     controlsState.aTarget = float(a_acc)
+    controlsState.decelForModelDEPRECATED = self.sm['longitudinalPlan'].decelForTurnDEPRECATED
     controlsState.cumLagMs = -self.rk.remaining * 1000.
     controlsState.startMonoTime = int(start_time * 1e9)
     controlsState.forceDecel = bool(force_decel)