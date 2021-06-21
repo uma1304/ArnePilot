027c6a48a59445b57f6e05dc0b3a54e5c29b8c1a
alfhern <hz.alfonso@gmail.com>
2021-06-21 11:06:07 +0200

    Simulator: Load simulator map data from OSM XML file

--- selfdrive/mapd/lib/osm.py
+++ selfdrive/mapd/lib/osm.py
@@ -1,6 +1,17 @@
 import overpy
 import numpy as np
 from selfdrive.mapd.lib.geo import R
+import os
+
+
+_SIM = "SIMULATION" in os.environ
+_DEBUG = True
+
+
+def _debug(msg):
+  if not _DEBUG:
+    return
+  print(msg)
 
 
 class OSM():
@@ -9,6 +20,9 @@ class OSM():
     # self.api = overpy.Overpass(url='http://3.65.170.21/api/interpreter')
 
   def fetch_road_ways_around_location(self, lat, lon, radius):
+    if _SIM:
+      return self.simulator_fetch()
+
     # Calculate the bounding box coordinates for the bbox containing the circle around location.
     bbox_angle = np.degrees(radius / R)
     # fetch all ways and nodes on this ways in bbox
@@ -26,4 +40,16 @@ class OSM():
       print(f'Exception while querying OSM:\n{e}')
       ways = []
 
+    return ways
+
+  def simulator_fetch(self):
+    _debug('OSM: Start simulator fetch')
+
+    path = os.getenv("CARLA_OVERPASS_XML_PATH", "../mapd/lib/carla_town04_osm_overpass.xml")
+
+    with open(path, 'r') as f:
+      overpass_xml = f.read()
+      ways = self.api.parse_xml(overpass_xml).ways
+
+    _debug('OSM: Finish simulator fetch')
     return ways
