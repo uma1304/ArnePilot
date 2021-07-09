cc14bb5c7ab8679b42ee717f27562ef0bc75776c
Arne Schwarck <arneschwarck@gmail.com>
2021-07-09 21:30:04 +0200

    No gas or brake when radar not working

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -481,6 +481,8 @@ class Controls:
 
     # Gas/Brake PID loop
     actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, plan.vTargetFuture, a_acc_sol, self.CP, self.sm, plan.hasLead, self.sm['radarState'], plan.decelForTurn, plan.longitudinalPlanSource)
+    if not self.sm['plan'].radarValid:
+      actuators.gas, actuators.brake = 0.,0.
     # Steering PID loop and lateral MPC
     actuators.steer, actuators.steerAngle, lac_log = self.LaC.update(self.active, CS, self.CP, path_plan)
 
