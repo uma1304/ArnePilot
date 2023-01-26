c3c041229082c14c0589b060c9a2b7c037138ce4
uma1304 <81022355+uma1304@users.noreply.github.com>
2023-01-26 20:07:40 +0100

    Fix Volkswagen

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -216,30 +216,16 @@ class CarState(CarStateBase):
     # preferences, including separate units for for distance vs. speed.
     # We use the speed preference for OP.
     self.displayMetricUnits = not pt_cp.vl["Einheiten_1"]["MFA_v_Einheit_02"]
-
-    # Stock FCW is considered active if a warning is displayed to the driver
-    # or the release bit for brake-jerk warning is set. Stock AEB considered
-    # active if the partial braking or target braking release bits are set.
-    # Ref: VW SSP 890253 "Volkswagen Driver Assistance Systems V2", "Front
-    # Assist with Braking: Golf Family" (applies to all MQB)
-    ret.stockFcw = False
-    ret.stockAeb = False
-
-    # Consume blind-spot radar info/warning LED states, if available
-    ret.leftBlindspot = False
-    ret.rightBlindspot = False
-
-    # Consume SWA (Lane Change Assist) relevant info from factory LDW message
-    # to pass along to the blind spot radar controller
+    
     self.ldw_lane_warning_left = False
     self.ldw_lane_warning_right = False
     self.ldw_side_dlc_tlc = False
-    self.ldw_dlc = 0
-    self.ldw_tlc = 0
-
+    self.ldw_dlc = False
+    self.ldw_tlc = False
+    
     # Update ACC radar status.
     # FIXME: This is unfinished and not fully correct, need to improve further
-    ret.cruiseState.available = bool(pt_cp.vl["GRA_neu"]['Hauptschalter'])
+    ret.cruiseState.available = bool(pt_cp.vl["GRA_Neu"]['GRA_Hauptschalt'])
     ret.cruiseState.enabled = True if pt_cp.vl["Motor_2"]['GRA_Status'] in [1, 2] else False
 
     # Update ACC setpoint. When the setpoint reads as 255, the driver has not
@@ -249,12 +235,12 @@ class CarState(CarStateBase):
       ret.cruiseState.speed = 0
 
     # Update control button states for turn signals and ACC controls.
-    self.buttonStates["accelCruise"] = bool(pt_cp.vl["GRA_neu"]['Kurz_Tip_up']) or bool(pt_cp.vl["GRA_neu"]['Lang_Tip_up'])
-    self.buttonStates["decelCruise"] = bool(pt_cp.vl["GRA_neu"]['Kurz_Tip_down']) or bool(pt_cp.vl["GRA_neu"]['Lang_Tip_down'])
-    self.buttonStates["cancel"] = bool(pt_cp.vl["GRA_neu"]['Abbrechen'])
-    self.buttonStates["setCruise"] = bool(pt_cp.vl["GRA_neu"]['Setzen'])
-    self.buttonStates["resumeCruise"] = bool(pt_cp.vl["GRA_neu"]['Wiederaufnahme'])
-    self.buttonStates["gapAdjustCruise"] = bool(pt_cp.vl["GRA_neu"]['Zeitlueckenverstellung'])
+    self.buttonStates["accelCruise"] = bool(pt_cp.vl["GRA_Neu"]['GRA_Up_kurz']) or bool(pt_cp.vl["GRA_Neu"]['GRA_Up_lang'])
+    self.buttonStates["decelCruise"] = bool(pt_cp.vl["GRA_Neu"]['GRA_Down_kurz']) or bool(pt_cp.vl["GRA_Neu"]['GRA_Down_lang'])
+    self.buttonStates["cancel"] = bool(pt_cp.vl["GRA_Neu"]['GRA_Abbrechen'])
+    self.buttonStates["setCruise"] = bool(pt_cp.vl["GRA_Neu"]['GRA_Neu_Setzen'])
+    self.buttonStates["resumeCruise"] = bool(pt_cp.vl["GRA_Neu"]['GRA_Recall'])
+    self.buttonStates["gapAdjustCruise"] = bool(pt_cp.vl["GRA_Neu"]['GRA_Zeitluecke'])
     ret.leftBlinker = bool(pt_cp.vl["Gate_Komf_1"]['GK1_Blinker_li'])
     ret.rightBlinker = bool(pt_cp.vl["Gate_Komf_1"]['GK1_Blinker_re'])
 
@@ -262,7 +248,7 @@ class CarState(CarStateBase):
     # to the radar. Ends up being different for steering wheel buttons vs
     # third stalk type controls.
     # TODO: Check to see what info we need to passthru and spoof on PQ
-    self.graHauptschalter = pt_cp.vl["GRA_neu"]['Hauptschalter']
+    self.graHauptschalter = pt_cp.vl["GRA_Neu"]['GRA_Hauptschalt']
     self.graTypHauptschalter = False
     self.graButtonTypeInfo = False
     self.graTipStufe2 = False
@@ -273,7 +259,7 @@ class CarState(CarStateBase):
 
     # Check to make sure the electric power steering rack is configured to
     # accept and respond to HCA_01 messages and has not encountered a fault.
-    self.steeringFault = pt_cp.vl["Lenkhilfe_2"]['LH2_Sta_HCA'] not in [3, 5]
+    self.steeringFault = pt_cp.vl["Lenkhilfe_2"]['LH2_Sta_HCA'] not in [3, 5, 7]
 
     return ret
 
@@ -404,15 +390,15 @@ class CarState(CarStateBase):
       ("MFA_v_Einheit_02", "Einheiten_1", 0),       # MPH vs KMH speed display
       ("Bremsinfo", "Kombi_1", 0),                  # Manual handbrake applied
       ("GRA_Status", "Motor_2", 0),                 # ACC engagement status
-      ("Hauptschalter", "GRA_neu", 0),              # ACC button, on/off
-      ("Abbrechen", "GRA_neu", 0),                  # ACC button, cancel
-      ("Setzen", "GRA_neu", 0),                     # ACC button, set
-      ("Lang_Tip_up", "GRA_neu", 0),                # ACC button, increase or accel, long press
-      ("Lang_Tip_down", "GRA_neu", 0),              # ACC button, decrease or decel, long press
-      ("Kurz_Tip_up", "GRA_neu", 0),                # ACC button, increase or accel, short press
-      ("Kurz_Tip_down", "GRA_neu", 0),              # ACC button, decrease or decel, short press
-      ("Wiederaufnahme", "GRA_neu", 0),             # ACC button, resume
-      ("Zeitlueckenverstellung", "GRA_neu", 0),     # ACC button, time gap adj
+      ("GRA_Hauptschalt", "GRA_Neu", 0),              # ACC button, on/off
+      ("GRA_Abbrechen", "GRA_Neu", 0),                  # ACC button, cancel
+      ("GRA_Neu_Setzen", "GRA_Neu", 0),                     # ACC button, set
+      ("GRA_Up_lang", "GRA_Neu", 0),                # ACC button, increase or accel, long press
+      ("GRA_Down_lang", "GRA_Neu", 0),              # ACC button, decrease or decel, long press
+      ("GRA_Up_kurz", "GRA_Neu", 0),                # ACC button, increase or accel, short press
+      ("GRA_Down_kurz", "GRA_Neu", 0),              # ACC button, decrease or decel, short press
+      ("GRA_Recall", "GRA_Neu", 0),             # ACC button, resume
+      ("GRA_Zeitluecke", "GRA_Neu", 0),     # ACC button, time gap adj
     ]
 
     checks = [
@@ -423,7 +409,7 @@ class CarState(CarStateBase):
       ("Motor_3", 100),           # From J623 Engine control module
       ("Airbag_1", 50),           # From J234 Airbag control module
       ("Bremse_5", 50),           # From J104 ABS/ESP controller
-      ("GRA_neu", 50),            # From J??? steering wheel control buttons
+      ("GRA_Neu", 50),            # From J??? steering wheel control buttons
       ("Kombi_1", 50),            # From J285 Instrument cluster
       ("Motor_2", 50),            # From J623 Engine control module
       ("Lenkhilfe_2", 20),        # From J500 Steering Assist with integrated sensors
@@ -462,7 +448,7 @@ class CarState(CarStateBase):
     checks = [
       # sig_address, frequency
       # FIXME: LDW_02 should be checked when we have better detection of installed hardware
-      #("LDW_02", 10),  # From R242 Driver assistance camera
+      ("LDW_02", 10),  # From R242 Driver assistance camera
     ]
 
     if CP.networkLocation == NWL.gateway:
